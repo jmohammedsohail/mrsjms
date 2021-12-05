a38d256646dc926e473d5347ef325ff9fbac6b83
jmohammedsohail <48152829+jmohammedsohail@users.noreply.github.com>
2021-12-05 19:05:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 
 import pickle
+import pandas as pd
 import streamlit as st
 import requests
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+# data = pickle.load(open('data.pkl','rb'))
+
+df1 = pd.read_csv('./model/netflix_titles.csv')
+
+df1['combined'] = df1['description']+df1['cast']+df1['director']
+# df1['combined'].head(5)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfv = TfidfVectorizer(min_df = 3,max_features = None,analyzer = 'word',token_pattern = 'r\w{1,}', ngram_range = (1,3), stop_words = 'english')
+
+#Import TfIdfVectorizer from scikit-learn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
+
+#Replace NaN with an empty string
+df1['combined'] = df1['combined'].fillna('')
+
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(df1['combined'])
+
+#Output the shape of tfidf_matrix
+# tfidf_matrix.shape
+
+
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
+
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
 
 
 def get_recommendations(movie):
@@ -11,13 +46,13 @@ def get_recommendations(movie):
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
     
-    recommended_movie_names=data['title'].iloc[movie_indices].values
+    recommended_movie_names=df1['title'].iloc[movie_indices].values
     
     return  recommended_movie_names
-# load the movie_list model,data, and tfidf vectorizer from disk
+# load the movie_list model from disk
 movies = pickle.load(open('movie_list.pkl','rb'))
-cosine_sim = pickle.load(open('cosine_sim.pkl','rb'))
-data = pickle.load(open('data.pkl','rb'))
+
+
 st.header('Netfilx Movie Recommender System')
 st.subheader('By Mohammed Sohail')
 
